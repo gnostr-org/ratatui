bb6d33344329f9dc1207412fc9441f2dec358399
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-02-25 21:51:04 -0500

    examples/libp2p_chat.rs:intermediate

--- examples/libp2p_chat.rs
+++ examples/libp2p_chat.rs
@@ -1,32 +1,3 @@
-//! # [Ratatui] User Input example
-//!
-//! The latest version of this example is available in the [examples] folder in the repository.
-//!
-//! Please note that the examples are designed to be run against the `main` branch of the Github
-//! repository. This means that you may not be able to compile with the latest release version on
-//! crates.io, or the one that you have installed locally.
-//!
-//! See the [examples readme] for more information on finding examples that match the version of the
-//! library you are using.
-//!
-//! [Ratatui]: https://github.com/ratatui/ratatui
-//! [examples]: https://github.com/ratatui/ratatui/blob/main/examples
-//! [examples readme]: https://github.com/ratatui/ratatui/blob/main/examples/README.md
-
-// A simple example demonstrating how to handle user input. This is a bit out of the scope of
-// the library as it does not provide any input handling out of the box. However, it may helps
-// some to get started.
-//
-// This is a very simple example:
-//   * An input box always focused. Every character you type is registered here.
-//   * An entered character is inserted at the cursor position.
-//   * Pressing Backspace erases the left character before the cursor position
-//   * Pressing Enter pushes the current input in the history of previous messages. **Note: ** as
-//   this is a relatively simple example unicode characters are unsupported and their use will
-// result in undefined behaviour.
-//
-// See also https://github.com/rhysd/tui-textarea and https://github.com/sayanarijit/tui-input/
-
 use color_eyre::Result;
 use ratatui::{
     crossterm::event::{self, Event, KeyCode, KeyEventKind},
@@ -37,8 +8,6 @@ use ratatui::{
     DefaultTerminal, Frame,
 };
 
-use ratatui::prelude::Widget;
-
 fn main() -> Result<()> {
     color_eyre::install()?;
     let terminal = ratatui::init();
@@ -48,6 +17,7 @@ fn main() -> Result<()> {
 }
 
 /// App holds the state of the application
+#[derive(Default)]
 struct App {
     /// Current value of the input box
     input: String,
@@ -57,9 +27,19 @@ struct App {
     input_mode: InputMode,
     /// History of recorded messages
     messages: Vec<String>,
+    state: AppState,
 }
 
+#[derive(Default, Clone, Copy, PartialEq, Eq)]
+enum AppState {
+    #[default]
+    Running,
+    Quitting,
+}
+
+#[derive(Default, Clone, Copy, PartialEq, Eq)]
 enum InputMode {
+    #[default]
     Normal,
     Editing,
 }
@@ -71,6 +51,7 @@ impl App {
             input_mode: InputMode::Normal,
             messages: Vec::new(),
             character_index: 0,
+            state: AppState::Running,
         }
     }
 
@@ -138,6 +119,10 @@ impl App {
         self.reset_cursor();
     }
 
+    pub fn quit(&mut self) {
+        self.state = AppState::Quitting;
+    }
+
     fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
         loop {
             terminal.draw(|frame| self.draw(frame))?;
@@ -148,8 +133,10 @@ impl App {
                         KeyCode::Char('e') => {
                             self.input_mode = InputMode::Editing;
                         }
-                        KeyCode::Char('q') => {
+                        KeyCode::Char('q') | KeyCode::Esc => {
                             return Ok(());
+                            //self.quit();
+
                         }
                         _ => {}
                     },
